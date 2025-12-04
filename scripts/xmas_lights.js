a09d9d888073081db86a4fd68339272a8ecb9c6a
BillyValt <123888056+BillyValt@users.noreply.github.com>
2025-12-04 04:33:06 +0300

    Add files via upload

---
+++ scripts/xmas_lights.js
@@ -0,0 +1,26 @@
+export default function runXmasLights() {
+  const colors = ["#ff3333", "#33ff55", "#33aaff", "#ffdd33", "#ff77ff"];
+  const lightsCount = 90; // количество огоньков
+
+  for (let i = 0; i < lightsCount; i++) {
+    const light = document.createElement("div");
+    light.classList.add("light");
+
+    // случайный размер
+    const size = Math.random() * 60 - 12;
+    light.style.width = size + "px";
+    light.style.height = size + "px";
+
+    // случайный цвет
+    light.style.background = colors[Math.floor(Math.random() * colors.length)];
+
+    // случайная позиция
+    light.style.left = Math.random() * 95 + "vw";
+    light.style.top = Math.random() * 95 + "vh";
+
+    // случайная задержка анимации (чтобы не мигали одновременно)
+    light.style.animationDelay = (Math.random() * 2) + "s";
+
+    document.body.appendChild(light);
+  }
+}
